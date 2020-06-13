2c18a4e35006a3d83c91b891767d3245f03d5761
Abigail Nielson <aa.nielson18@gmail.com>
2020-06-13 09:22:50 -0600

    worked on incorporating a scrolling navbar. added some styling to the library filter

--- public/js/index.js
+++ public/js/index.js
@@ -97,3 +97,20 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrolltop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navlinks").style.top = "0";
+    document.getElementById("navlinks").style.height = "50px";
+    // document.getElementById("logo").style.display = "none";
+    // document.getElementById("logo2").style.display = "block";
+    document.getElementById("navlinks").style.backgroundColor = "lightgray";
+  
+  } else {
+    document.getElementById("navlinks").style.backgroundColor = "transparent";
+    document.getElementById("logo").style.display = "block";
+    // document.getElementById("logo2").style.display = "none";
+  }
+}
