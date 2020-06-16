87502804bd76955c294a97611ffb1b3b9cb932b9
cftgithub <chiafongt@gmail.com>
2020-06-16 07:34:43 -0600

    fixed routes for needhelp_controller.js

--- controllers/needhelp_controller.js
+++ controllers/needhelp_controller.js
@@ -2,56 +2,60 @@
 
 var db = require("../models");
 
-module.exports = function(app) {
-    // GET route to get all the help requests
-    app.get("/api/needhelp", function(req, res) {
-<<<<<<< HEAD
-        db.//model name of db for need help page
-        .findAll({}).then(function(data) {
-=======
-        db.Help.findAll({}).then(function(data) {
->>>>>>> 541cd1770efc1a1f7572ede7ff698b7c749c4c45
-            res.json(data);
-        });
+module.exports = function (app) {
+  // GET route to get all the help requests
+  app.get("/needhelp", function (req, res) {
+    db.Help.findAll({}).then(function (data) {
+      res.render("helpBlog", { help: data });
     });
+  });
 
-    // POST route to save help requests
-    app.post("/api/needhelp", function(req, res) {
-        db.//model name of db for need help page
-        .create({
-          subject: req.body.subject,
-          body: req.body.body
-          //Any other columns?
-        }).then(function(data) {
-          res.json(data);
-        });
+  app.get("/form", function (req, res) {
+    db.Help.findAll({}).then(function (data) {
+      res.render("helpForm", { help: data });
     });
+  });
 
-    // DELETE route to delete help requests
-    app.delete("/api/needhelp/:id", function(req, res) {
-        db.//model name of db for need help page
-        .destroy({
-          where: {
-            id: req.params.id
-          }
-        }).then(function(data) {
-            res.json(data);
-        });
+  app.get("/addUser", function (req, res) {
+    db.Help.findAll({}).then(function (data) {
+      res.render("helpForm", { help: data });
     });
+  });
 
-    // PUT route to update help requests
-    app.put("/api/needhelp", function(req, res) {
-        db.//model name of db for need help pag
-        .update({
-          subject: req.body.subject,
-          body: req.body.body
-          //Any other columns?
-        }, {
-          where: {
-            id: req.body.id
-          }
-        }).then(function(data) {
-            res.json(data);
-        });
+  // POST route to save help requests
+  app.post("/needhelp", function (req, res) {
+    db.Help.create({
+      title: req.body.title,
+      body: req.body.body,
+      userId: req.body.userId
+    }).then(function (data) {
+      res.render(data);
     });
+  });
+
+  // DELETE route to delete help requests
+  // app.delete("/needhelp/:id", function (req, res) {
+  //     db.Help.destroy({
+  //         where: {
+  //             id: req.params.id
+  //         }
+  //     }).then(function (data) {
+  //         res.json(data);
+  //     });
+  // });
+
+  // PUT route to update help requests
+  // app.put("/needhelp", function (req, res) {
+  //     db.Help.update({
+  //         subject: req.body.subject,
+  //         body: req.body.body
+  //         //Any other columns?
+  //     }, {
+  //         where: {
+  //             id: req.body.id
+  //         }
+  //     }).then(function (data) {
+  //         res.json(data);
+  //     });
+  // });
 };
