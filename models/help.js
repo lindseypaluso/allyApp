2fb7d9550f82caa5477eaae2776ca9db3e5a6470
cftgithub <chiafongt@gmail.com>
2020-06-16 18:43:42 -0600

    modified handlebars file name, updated help model, added js files for Needhelp tab

--- models/help.js
+++ models/help.js
@@ -1,8 +1,28 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Help = sequelize.define("Help", {
-        title: DataTypes.STRING,
-        body: DataTypes.STRING,
-        userId: DataTypes.STRING
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        }
     });
+
+
+    Help.associate = function (models) {
+        // A Help model can't be created without a Username
+        Help.belongsTo(models.Username, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
     return Help;
 };
