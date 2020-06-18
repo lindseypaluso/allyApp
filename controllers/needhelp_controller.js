40af928e2bb02d3616747aee9018b9e875b81102
cftgithub <chiafongt@gmail.com>
2020-06-18 16:38:00 -0600

    cleaned up unnecessary code for Need Help tab.

--- controllers/needhelp_controller.js
+++ controllers/needhelp_controller.js
@@ -10,39 +10,39 @@ module.exports = function (app) {
   });
 
   // POST route to save help requests
-  app.post("/needhelp", function (req, res) {
-    db.Help.create({
-      subject: req.body.subject,
-      body: req.body.body
-      //Any other columns?
-    }).then(function (data) {
-      res.json(data);
-    });
-  });
+  // app.post("/helpBlog", function (req, res) {
+  //   db.Help.create({
+  //     subject: req.body.subject,
+  //     body: req.body.body
+  //     //Any other columns?
+  //   }).then(function (data) {
+  //     res.render(data);
+  //   });
+  // });
 
   // DELETE route to delete help requests
-  app.delete("/needhelp/:id", function (req, res) {
-    db.Help.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function (data) {
-      res.json(data);
-    });
-  });
+  // app.delete("/needhelp/:id", function (req, res) {
+  //   db.Help.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   }).then(function (data) {
+  //     res.json(data);
+  //   });
+  // });
 
   // PUT route to update help requests
-  app.put("/needhelp", function (req, res) {
-    db.Help.update({
-      subject: req.body.subject,
-      body: req.body.body
-      //Any other columns?
-    }, {
-      where: {
-        id: req.body.id
-      }
-    }).then(function (data) {
-      res.json(data);
-    });
-  });
+  // app.put("/needhelp", function (req, res) {
+  //   db.Help.update({
+  //     subject: req.body.subject,
+  //     body: req.body.body
+  //     //Any other columns?
+  //   }, {
+  //     where: {
+  //       id: req.body.id
+  //     }
+  //   }).then(function (data) {
+  //     res.json(data);
+  //   });
+  // });
 };
