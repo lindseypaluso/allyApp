f6284a6fd435385feb72fd244dd894d90383f10d
Abigail Nielson <aa.nielson18@gmail.com>
2020-06-18 19:46:08 -0600

    finalized data pulls from the controller. finished bootstrap responsiveness

--- controllers/library_controller.js
+++ controllers/library_controller.js
@@ -1,33 +1,34 @@
 var db = require("../models");
 var books, articles, childrensBooks, podcasts, towatch;
 
-module.exports = function(app) {
-    app.get("/library", function(req, res) {
-        db.Articles.findAll({}).then(function(data) {
+module.exports = function (app) {
+    app.get("/library", function (req, res) {
+        db.Articles.findAll({}).then(function (data) {
             //res.json(data);
             articles = data;
-        });
 
-        db.Books.findAll({}).then(function(data) {
-            books = data;
-        });
+            db.Books.findAll({}).then(function (data) {
+                books = data;
 
-        db.ChildrensBooks.findAll({}).then(function(data) {
-            //res.json(data);
-            childrensBooks = data;
-        });
+                db.ChildrensBooks.findAll({}).then(function (data) {
+                    //res.json(data);
+                    childrensBooks = data;
 
-        db.Podcasts.findAll({}).then(function(data) {
-            //res.json(data);
-            podcasts = data;
-        });
+                    db.Podcasts.findAll({}).then(function (data) {
+                        //res.json(data);
+                        podcasts = data;
 
-        // STILL WORKING ON THIS ONE -MEGAN
-        db.ToWatch.findAll({}).then(function(data) {
-            //res.json(data);
-            towatch = data;
+                        db.ToWatch.findAll({}).then(function (data) {
+                            //res.json(data);
+                            towatch = data;
+
+                            console.log(books);
+                            res.render("library", { books, childrensBooks, articles, podcasts, towatch });
+                        });
+                    });
+
+                });
+            });
         });
-        console.log(books);
-        res.render("library", { books, childrensBooks,  articles, podcasts, towatch});
     });
 };
