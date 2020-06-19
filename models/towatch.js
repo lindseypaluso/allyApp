ca1601288f2be479b31cea09ede36b20aae91e2f
Lindsey Paluso <lindzymarie@gmail.com>
2020-06-18 21:42:40 -0600

    fixed, going to test deploy

--- models/towatch.js
+++ models/towatch.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
     var ToWatch = sequelize.define("ToWatch", {
         title: DataTypes.STRING,
-        streaming_service: DataTypes.STRING,
+        source: DataTypes.STRING,
         link1: DataTypes.STRING,
         link2: DataTypes.STRING
     });
