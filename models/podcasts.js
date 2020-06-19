f088b50e419e3cf39a5830543dda325e030931c2
Lindsey Paluso <lindzymarie@gmail.com>
2020-06-18 20:02:20 -0600

    final db

--- models/podcasts.js
+++ models/podcasts.js
@@ -2,7 +2,8 @@ module.exports = function(sequelize, DataTypes) {
   var Podcasts = sequelize.define("Podcasts", {
     title: DataTypes.STRING,
     description: DataTypes.STRING,
-    link: DataTypes.STRING
+    link1: DataTypes.STRING,
+    link2: DataTypes.STRING
   });
   return Podcasts;
 };
